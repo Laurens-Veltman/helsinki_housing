62e3b45262a50aafc7bcaa5449c15625ffaa3b43
Laurens-Veltman <96501346+Laurens-Veltman@users.noreply.github.com>
2023-10-21 18:46:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,41 @@
 import streamlit as st
 import pandas as pd
 
+import json
+from shapely.geometry import Polygon, Point
+
 #path = "data/helsinki.geo.json"
-path = "https://github.com/dhh16/helsinki/blob/master/osaalueet.geojson?short_path=c666dcd"
-with open(path) as f:
-  geojson_data = f.read()
+#path = "https://github.com/dhh16/helsinki/blob/master/osaalueet.geojson?short_path=c666dcd"
+#with open(path) as f:
+#  geojson_data = f.read()
 
-st.write(geojson_data)
-#st.map(geojson_data)
+class DistrictJSON:
+    def __init__(self, filename):
+        self._filename = filename
+        self._poly_dict = {}
+
+        self.load()
+
+    def load(self):
+        with open(self._filename) as f:
+            data = json.loads(f.read())
+
+            for shape in data['features']:
+                id = int(shape['properties']['TUNNUS'])
+                coords = shape['geometry']['coordinates'][0]
+                self._poly_dict[id] = Polygon(coords)
+
+    def get_polygon(self, id):
+        return self._poly_dict[id]
 
 
+def main():
+    file = "https://github.com/dhh16/helsinki/blob/master/osaalueet.geojson?short_path=c666dcd"
+    districts = DistrictJSON(file)
+    districts.load()
+    print(districts.get_polygon(171))
+    print(districts.get_polygon(171).contains(Point(24.92046539288323, 60.20190764575884)))
+    #st.map(geojson_data)
+
+if __name__ == '__main__':
+    main()
