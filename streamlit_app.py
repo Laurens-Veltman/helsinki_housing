268731733576697a8a356ece88cc16eacf81d35a
Laurens-Veltman <96501346+Laurens-Veltman@users.noreply.github.com>
2023-10-21 18:37:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from streamlit_folium import folium_static
 
 #path = "data/helsinki.geo.json"
 path = "https://github.com/dhh16/helsinki/blob/master/osaalueet.geojson?short_path=c666dcd"
