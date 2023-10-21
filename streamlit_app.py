234130b3a20e7e6abd85fa4cdbdf8ac278b0878c
Laurens-Veltman <96501346+Laurens-Veltman@users.noreply.github.com>
2023-10-21 18:24:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import pandas as pd
+from streamlit_folium import folium_static
 
 path = "data/helsinki.geo.json"
 with open(path) as f:
   geojson_data = f.read()
 
-st.map(geojson_data)
+#st.map(geojson_data)
+
+# Create a folium map centered at a specific location
+m = folium.Map(location=[0, 0], zoom_start=2)
+
+# Add the GeoJSON data to the map
+folium.GeoJson(geojson_data, name='geojson').add_to(m)
+
+# Display the map using Streamlit
+st.title('GeoJSON Visualization with Folium in Streamlit')
+folium_static(m)
