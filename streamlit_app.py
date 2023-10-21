45849b435cdf911e8bf7cc95f1f28112d52c7c7f
Laurens-Veltman <96501346+Laurens-Veltman@users.noreply.github.com>
2023-10-21 19:15:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,12 +32,7 @@ class DistrictJSON:
 def main():
     url = "https://github.com/dhh16/helsinki/blob/master/osaalueet.geojson"
     file = requests.get(url)
-    #file = "data/helsinki.geo.json"
-    districts = DistrictJSON(file.geojson)
-    districts.load()
-    #print(districts.get_polygon(171))
-    #print(districts.get_polygon(171).contains(Point(24.92046539288323, 60.20190764575884)))
-    #st.map(geojson_data)
+    st.write(file)
 
 if __name__ == '__main__':
     main()
